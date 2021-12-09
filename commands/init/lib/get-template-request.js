acc0925a0d67d221aaa6c7c464a3cba080e8cf76
pearone <1181019452@qq.com>
2021-12-09 21:17:41 +0800

    no backend

--- commands/init/lib/get-template-request.js
+++ commands/init/lib/get-template-request.js
@@ -1,7 +1,26 @@
 const request = require("@dpd-cli/request");
 
+// module.exports = () => {
+//   return request({
+//     url: "/template",
+//   });
+// };
+
 module.exports = () => {
-  return request({
-    url: "/template",
-  });
+  return [
+    {
+      description: "项目基础模版",
+      name: "dpd-cli-template",
+      version: "1.0.0",
+      type: "normal",
+      installCommand: "npm install",
+      startCommand: "npm run dev",
+    },
+    {
+      description: "管理后台模版",
+      name: "dpd-cli-template",
+      version: "1.0.0",
+      type: "custom",
+    },
+  ];
 };
