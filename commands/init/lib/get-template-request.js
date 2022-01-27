d191abb6e582eda3c59b9c33f80e1f9c17d72be0
pearone <1181019452@qq.com>
2022-01-27 13:06:35 +0800

    update

--- commands/init/lib/get-template-request.js
+++ commands/init/lib/get-template-request.js
@@ -10,7 +10,7 @@ module.exports = () => {
   return Promise.resolve([
     {
       description: "项目基础模版",
-      name: "dpd-cli-template",
+      name: "pear-cli-base-template",
       version: "1.0.0",
       type: "normal", // normal、custom
       installCommand: "npm install",
